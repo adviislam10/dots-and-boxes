810c045a6dbff57d6976ffaa19d59c401471cbee
adviislam10 <71040123+adviislam10@users.noreply.github.com>
2020-10-29 14:39:19 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // Constants and global variables
 const NUM_ROWS = 15;
 const NUM_COLS = 15;
-let playerturn = 0;
 
 // Create an array to represent a grid
 let divs = createDivsArray();
@@ -23,21 +22,13 @@ function playerStart() {
 }
 
 //Player Turn Process
-if (playerturn == 1) {
-    document.getElementById('playerdisp').innerHTML = 'PLAYER 1 TURN';
-    document.getElementById('playerdisp').classList.add('player1turn');
-} else if(playerturn == 2) {
-    document.getElementById('playerdisp').innerHTML = 'PLAYER 2 TURN';
-    document.getElementById('playerdisp').classList.add('player2turn');
-}
+
 
 // Set Check Functions
 checkTop = function (row, col) {
     if ((divs[row][col] == 4) && (divs[row - 2][col] == 4) && (divs[row - 1][col - 1] == 4) && (divs[row - 1][col + 1] == 4) && (divs[row - 1][col] == 3)) {
         document.getElementById('cell' + (row - 1) + '-' + col).classList.add('square');
-    } else {
-        playerturn++
-    }
+    } 
 }
 
 checkBottom = function (row, col) {
