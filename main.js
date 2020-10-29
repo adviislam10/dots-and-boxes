00319237bb7847c5658a96a179d331c4ec32f345
adviislam10 <71040123+adviislam10@users.noreply.github.com>
2020-10-29 15:21:53 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Constants and global variables
 const NUM_ROWS = 15;
 const NUM_COLS = 15;
+let playerturn = 1;
 
 // Create an array to represent a grid
 let divs = createDivsArray();
@@ -8,43 +9,68 @@ let divs = createDivsArray();
 // Create divs to model the grid array
 createDivGrid(divs);
 
-// Set Players
-document.getElementById('start').addEventListener('click', playerStart);
-
-function playerStart() {
-    // Remove instructions and button
-    document.getElementById('removeI').remove();
-    document.getElementById('start').remove();
-
+//Player Turn Process
+if (playerturn == 1) {
     // Add player turn
     document.getElementById('playerdisp').innerHTML = 'PLAYER 1 TURN';
     document.getElementById('playerdisp').classList.add('player1turn');
+} else if (playerturn == 2) {
+    // Add player turn
+    document.getElementById('playerdisp').innerHTML = 'PLAYER 2 TURN';
+    document.getElementById('playerdisp').classList.add('player2turn');
 }
-
-//Player Turn Process
-
 
 // Set Check Functions
 checkTop = function (row, col) {
     if ((divs[row][col] == 4) && (divs[row - 2][col] == 4) && (divs[row - 1][col - 1] == 4) && (divs[row - 1][col + 1] == 4) && (divs[row - 1][col] == 3)) {
-        document.getElementById('cell' + (row - 1) + '-' + col).classList.add('square');
-    } 
+        if (playerturn == 1) {
+            document.getElementById('cell' + (row - 1) + '-' + col).classList.add('square1');
+        } else {
+            document.getElementById('cell' + (row - 1) + '-' + col).classList.add('square2');
+        }
+    } else {
+        changePlayer();
+    }
 }
 
 checkBottom = function (row, col) {
     if ((divs[row][col] == 4) && (divs[row + 2][col] == 4) && (divs[row + 1][col - 1] == 4) && (divs[row + 1][col + 1] == 4) && (divs[row + 1][col] == 3)) {
-        document.getElementById('cell' + (row + 1) + '-' + col).classList.add('square');
+        if (playerturn == 1) {
+            document.getElementById('cell' + (row - 1) + '-' + col).classList.add('square1');
+        } else {
+            document.getElementById('cell' + (row - 1) + '-' + col).classList.add('square2');
+        }
+    } else {
+        changePlayer();
     }
 }
 
+
 checkRight = function (row, col) {
     if ((divs[row][col] == 4) && (divs[row][col + 2] == 4) && (divs[row + 1][col + 1] == 4) && (divs[row - 1][col + 1] == 4) && (divs[row][col + 1] == 3)) {
-        document.getElementById('cell' + row + '-' + (col + 1)).classList.add('square');
+        if (playerturn == 1) {
+            document.getElementById('cell' + (row - 1) + '-' + col).classList.add('square1');
+        } else {
+            document.getElementById('cell' + (row - 1) + '-' + col).classList.add('square2');
+        }
+    } else {
+        changePlayer();
     }
 }
 
 checkLeft = function (row, col) {
     if ((divs[row][col] == 4) && (divs[row][col - 2] == 4) && (divs[row + 1][col - 1] == 4) && (divs[row - 1][col - 1] == 4) && (divs[row][col - 1] == 3)) {
-        document.getElementById('cell' + row + '-' + (col - 1)).classList.add('square');
+        if (playerturn == 1) {
+            document.getElementById('cell' + (row - 1) + '-' + col).classList.add('square1');
+        } else {
+            document.getElementById('cell' + (row - 1) + '-' + col).classList.add('square2');
+        }
+    } else {
+        changePlayer();
     }
+}
+
+// Change Player
+function changePlayer() {
+
 }
